ae773871b504bf6d4ebb86aec5460443f2cc08a7
leonso <leo@orderhero.us>
2019-12-29 22:36:22 -0800

    init

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import App from './components/refsTypes/stringRef';
+// import App from './components/refsTypes/callbackRef';
+// import App from './components/refsTypes/createRef';
+// import App from './components/refsTypes/useRef';
+// import App from './components/refsTypes/instanceRef';
+
+// import App from './components/childrenRef/functional';
+// import App from './components/childrenRef/class';
+import App from './components/childrenRef/hoc';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
